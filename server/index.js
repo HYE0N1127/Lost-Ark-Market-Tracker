1194a82114a8c4a9adc7a6acd5f40b3e3c5b5600
현빈 <hhb041127@naver.com>
2025-11-12 01:43:55 +0900

    modify: 시간을 불러오는 로직을 함수로 변경

--- server/index.js
+++ server/index.js
@@ -4,41 +4,53 @@ import { jewel } from "./jewel/jewel.js";
 import { gem } from "./gem/gem.js";
 import cron from "node-cron";
 
-const app = express();
+const server = express();
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
-  console.log("서버를 종료하려면 Ctrl+C를 누르세요.");
-});
+class App {
+  constructor() {
+    server.listen(port, () => {
+      console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
+      console.log("서버를 종료하려면 Ctrl+C를 누르세요.");
+    });
 
-const engraveData = engrave.getMarketData();
-const blazingJewelData = jewel.getBlazingJewelData();
-const doomfireJewelData = jewel.getBlazingJewelData();
-const gemData = gem.getGemData();
+    this.getMarketData();
+    this.fetchData();
+  }
 
-cron.schedule("*/1 * * * *", () => {
-  try {
+  #getDate() {
     const today = new Date();
 
     const hours = ("0" + today.getHours()).slice(-2);
     const minutes = ("0" + today.getMinutes()).slice(-2);
     const seconds = ("0" + today.getSeconds()).slice(-2);
 
-    const timeString = hours + ":" + minutes + ":" + seconds;
+    return hours + ":" + minutes + ":" + seconds;
+  }
 
+  getMarketData() {
     const engraveData = engrave.getMarketData();
     const blazingJewelData = jewel.getBlazingJewelData();
     const doomfireJewelData = jewel.getBlazingJewelData();
     const gemData = gem.getGemData();
 
-    console.log(`----- ${timeString} -----`);
+    console.log(`----- ${this.#getDate()} -----`);
     console.log(`engraveData: ${engraveData}`);
     console.log(`blazingJewelData: ${blazingJewelData}`);
     console.log(`doomfireJewelData: ${doomfireJewelData}`);
     console.log(`gemData: ${gemData}`);
-  } catch (err) {
-    console.error(`데이터 업데이트중 에러 발생: ${err}`);
   }
-});
+
+  fetchData() {
+    cron.schedule("*/1 * * * *", () => {
+      try {
+        this.getMarketData();
+      } catch (err) {
+        console.error(`데이터 업데이트중 에러 발생: ${err}`);
+      }
+    });
+  }
+}
+
+export const app = new App();
